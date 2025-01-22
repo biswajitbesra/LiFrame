f80537640cbd9bb47ea80931265846855f514fd3
biswajitbesra <biswajitbesra28@gmail.com>
2025-01-22 22:04:24 +0530

    Update

--- src/components/Pages/Layout/OverscrollBehavior.jsx
+++ src/components/Pages/Layout/OverscrollBehavior.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function OverscrollBehavior() {
     return (
         <div className='h-full overflow-scroll'>
-            <h1 className='text-3xl font-semibold mt-4 mb-6'>Overscroll Behavior--</h1>
+            <h1 className='text-3xl font-semibold mt-4 mb-6'>Overscroll Behavior.</h1>
             <p className="mb-4">The <span className="font-semibold">overscroll-behavior</span> property in LiFrame controls how the browser handles scrolling past the edge of a scrollable element. It is especially useful for managing scroll behavior in certain elements such as when a user scrolls beyond the end of a scrollable area. This can improve user experience by preventing unwanted scrolling effects like bouncing or content sliding when it shouldn't.</p>
             <section className='mb-7'>
                 <table className='w-full'>
@@ -55,22 +55,54 @@ function OverscrollBehavior() {
             </section>
             <section className="mb-7">
                 <h5 className='font-semibold text-lg'>Basic usage</h5>
-                <p className="mb-4"><span className="font-semibold">overscroll-behavior</span></p>
-                <pre className='border border-black p-2 rounded-lg'>
-                    <code>
-                        {`<div class="`}<span className="bg-secondary p-1 rounded">overscroll-contain</span>{` ...">Well, let me tell you something, ...</div>`}
-                    </code>
-                </pre>
-                <pre className='border border-black p-2 rounded-lg'>
-                    <code>
-                        {`<div class="`}<span className="bg-secondary p-1 rounded">overscroll-none</span>{` ...">Well, let me tell you something, ...</div>`}
-                    </code>
-                </pre>
-                <pre className='border border-black p-2 rounded-lg'>
-                    <code>
-                        {`<div class="`}<span className="bg-secondary p-1 rounded">overscroll-auto</span>{` ...">Well, let me tell you something, ...</div>`}
-                    </code>
-                </pre>
+                <div className="mb-7">
+                    <h6 className="font-bold text-lg mb-2">Preventing parent overscrolling</h6>
+                    <p className="mb-4">Use the <span className="font-semibold">overscroll-contain</span> utility to prevent scrolling in the target area from triggering scrolling in the parent element, but preserve “bounce” effects when scrolling past the end of the container in operating systems that support it.</p>
+                    <div className="h-60 border border-black rounded-lg mb-5 p-1">
+                        <div className='h-full p-5 space-y-2 overflow-auto overscroll-contain'>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum aliquam, corrupti saepe tempora suscipit laboriosam voluptatibus similique quaerat magnam quae qui debitis culpa recusandae praesentium aut, voluptas libero laudantium. Laborum soluta nesciunt sed. Ratione neque assumenda rem in cum.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla soluta, sequi rem, perspiciatis impedit repellendus nobis blanditiis corporis possimus velit voluptate accusamus. Repellendus porro magni accusamus repellat id dolorum illum obcaecati placeat possimus, velit recusandae tempora laudantium et sapiente doloribus commodi aut beatae. Corrupti at porro ipsum totam optio quam!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, eius. Molestiae illum accusantium sit praesentium, quis harum sunt assumenda non dolor saepe, delectus ea repellendus. Dolorem nobis ipsa quod ullam.</p>
+                        </div>
+                    </div>
+                    <pre className='border border-black p-2 rounded-lg'>
+                        <code>
+                            {`<div class="overscroll-auto `}<span className="bg-secondary p-1 rounded">overscroll-contain</span>{` ...">Lorem, ipsum dolor sit ...</div>`}
+                        </code>
+                    </pre>
+                </div>
+                <div className="mb-7">
+                    <h6 className="font-bold text-lg mb-2">Preventing overscroll bouncing</h6>
+                    <p className="mb-4">Use the <span className="font-semibold">overscroll-none</span> utility to prevent scrolling in the target area from triggering scrolling in the parent element, and also prevent “bounce” effects when scrolling past the end of the container.</p>
+                    <div className="h-60 border border-black rounded-lg mb-5 p-1">
+                        <div className='h-full p-5 space-y-2 overflow-auto overscroll-none'>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum aliquam, corrupti saepe tempora suscipit laboriosam voluptatibus similique quaerat magnam quae qui debitis culpa recusandae praesentium aut, voluptas libero laudantium. Laborum soluta nesciunt sed. Ratione neque assumenda rem in cum.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla soluta, sequi rem, perspiciatis impedit repellendus nobis blanditiis corporis possimus velit voluptate accusamus. Repellendus porro magni accusamus repellat id dolorum illum obcaecati placeat possimus, velit recusandae tempora laudantium et sapiente doloribus commodi aut beatae. Corrupti at porro ipsum totam optio quam!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, eius. Molestiae illum accusantium sit praesentium, quis harum sunt assumenda non dolor saepe, delectus ea repellendus. Dolorem nobis ipsa quod ullam.</p>
+                        </div>
+                    </div>
+                    <pre className='border border-black p-2 rounded-lg'>
+                        <code>
+                            {`<div class="overscroll-auto `}<span className="bg-secondary p-1 rounded">overscroll-none</span>{` ...">Lorem, ipsum dolor sit ...</div>`}
+                        </code>
+                    </pre>
+                </div>
+                <div className="mb-7">
+                    <h6 className="font-bold text-lg mb-2">Using the default overscroll behavior</h6>
+                    <p className="mb-4">Use the <span className="font-semibold">overscroll-auto</span> utility to make it possible for the user to continue scrolling a parent scroll area when they reach the boundary of the primary scroll area.</p>
+                    <div className="h-60 border border-black rounded-lg mb-5 p-1">
+                        <div className='h-full p-5 space-y-2 overflow-auto overscroll-auto'>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum aliquam, corrupti saepe tempora suscipit laboriosam voluptatibus similique quaerat magnam quae qui debitis culpa recusandae praesentium aut, voluptas libero laudantium. Laborum soluta nesciunt sed. Ratione neque assumenda rem in cum.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla soluta, sequi rem, perspiciatis impedit repellendus nobis blanditiis corporis possimus velit voluptate accusamus. Repellendus porro magni accusamus repellat id dolorum illum obcaecati placeat possimus, velit recusandae tempora laudantium et sapiente doloribus commodi aut beatae. Corrupti at porro ipsum totam optio quam!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, eius. Molestiae illum accusantium sit praesentium, quis harum sunt assumenda non dolor saepe, delectus ea repellendus. Dolorem nobis ipsa quod ullam.</p>
+                        </div>
+                    </div>
+                    <pre className='border border-black p-2 rounded-lg'>
+                        <code>
+                            {`<div class="overscroll-auto `}<span className="bg-secondary p-1 rounded">overscroll-auto</span>{` ...">Lorem, ipsum dolor sit ...</div>`}
+                        </code>
+                    </pre>
+                </div>
             </section>
             <section className='mb-7'>
                 <h5 className='font-semibold text-lg'>Applying conditionally</h5>
