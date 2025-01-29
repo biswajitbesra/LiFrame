d4ebfa84c786d890f606175ef59f995e91dbe16a
biswajitbesra <biswajitbesra28@gmail.com>
2025-01-29 21:28:45 +0530

    Message

--- src/components/Pages/Documentation/Documentation.jsx
+++ src/components/Pages/Documentation/Documentation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Documentation() {
     return (
         <div className='my-8'>
-            installing...
+            Change the color scheme
         </div>
     )
 }
