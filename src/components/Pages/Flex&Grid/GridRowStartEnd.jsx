f349826051280f17e7d5ae1c0f5cfe469a4c1d40
biswajitbesra <biswajitbesra28@gmail.com>
2025-02-17 21:23:08 +0530

    Page added

--- src/components/Pages/Flex&Grid/GridRowStartEnd.jsx
+++ src/components/Pages/Flex&Grid/GridRowStartEnd.jsx
@@ -2,8 +2,200 @@ import React from 'react'
 
 function GridRowStartEnd() {
     return (
-        <div>
-            grid row start / end
+        <div className='h-full overflow-scroll'>
+            <h1 className='text-3xl font-semibold mt-4 mb-6'>Grid Row Start / End.</h1>
+            <p className='mb-4'>The <span className='font-semibold'>grid-row</span> utility in LiFrame allows you to control how an element spans across grid rows. It helps define explicit start and end positions for grid items within a grid container.</p>
+
+            <section className='mb-7 h-3/5 overflow-y-scroll'>
+                <table className='w-full'>
+                    <thead className='sticky top-0 w-full bg-primary'>
+                        <tr>
+                            <th className='text-left border-b border-[#ababab] py-2'> Class </th>
+                            <th className='text-left border-b border-[#ababab] py-2'> Properties </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-auto</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: auto;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-1</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 1 / span 1;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-2</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 2 / span 2;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-3</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 3 / span 3;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-4</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 4 / span 4;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-5</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 5 / span 5;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-6</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 6 / span 6;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-7</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 7 / span 7;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-8</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 8 / span 8;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-9</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 9 / span 9;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-10</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 10 / span 10;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-11</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 11 / span 11;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-12</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: span 12 / span 12;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-span-full</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row: 1 / -1;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-1</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 1;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-2</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 2;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-3</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 3;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-4</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 4;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-5</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 5;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-6</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 6;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-7</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 7;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-8</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 8;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-9</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 9;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-10</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 10;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-11</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 11;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-12</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 12;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-13</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: 13;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-start-auto</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-start: auto;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-1</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 1;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-2</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 2;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-3</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 3;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-4</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 4;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-5</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 5;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-6</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 6;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-7</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 7;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-8</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 8;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-9</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 9;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-10</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 10;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-11</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 11;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-12</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 12;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-13</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: 13;</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-[#ababab] py-2">row-end-auto</td>
+                            <td class="border-b border-[#ababab] py-2">grid-row-end: auto;</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+
+            <section className='mb-7'>
+                <h5 className='font-semibold text-lg'>Basic usage</h5>
+                <p className='mb-4'>Use the <span className='font-semibold'>row-start-&#9913;</span> and <span className='font-semibold'>row-end-&#9913;</span> utilities to specify where a grid item should start and end. For example, <span className='font-semibold'>row-start-2 row-end-4</span> makes an item start at the second row line and end at the fourth.</p>
+            </section>
+
+            <section className='mb-7'>
+                <h5 className='font-semibold text-lg'>Applying conditionally</h5>
+                <p className='mb-4'>You can use responsive prefixes like <span className='font-semibold'>lg:row-span-3</span> to dynamically change how rows are spanned at different screen sizes. Additionally, pseudo-classes like <span className='font-semibold'>hover</span> and <span className='font-semibold'>focus</span> can be used to modify row positioning interactively.</p>
+            </section>
         </div>
     )
 }
